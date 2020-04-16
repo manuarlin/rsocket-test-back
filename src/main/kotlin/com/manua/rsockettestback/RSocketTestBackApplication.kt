4ed4a2fb9c50cd164d4718e008f95b1a46760365
Manuel Arlin <manuel.arlin-ext@pole-emploi.fr>
2020-04-16 21:42:28 +0200

    Add resume feature

--- src/main/kotlin/com/manua/rsockettestback/RSocketTestBackApplication.kt
+++ src/main/kotlin/com/manua/rsockettestback/RSocketTestBackApplication.kt
@@ -1,11 +1,17 @@
 package com.manua.rsockettestback
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.rsocket.server.ServerRSocketFactoryProcessor
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 
 @SpringBootApplication
-class RSocketTestBackApplication
+class RSocketTestBackApplication {
+
+    @Bean
+    fun processor(): ServerRSocketFactoryProcessor = ServerRSocketFactoryProcessor { factory -> factory.resume() }
+}
 
 fun main(args: Array<String>) {
     runApplication<RSocketTestBackApplication>(*args)
